e057b38f3db33baa1cb528ce5a20b6d0ac1c4dd7
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-05-13 17:59:33 -0400

    Write a function that plays one round of Rock Paper Scissors.

--- javascript.js
+++ javascript.js
@@ -10,3 +10,30 @@ function getComputerChoice() {
     }
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "rock") {
+        if (computerSelection === "paper") {
+            return "You Lose! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Win! Rock beats Scissors";
+        }
+    }
+    else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "You Won! Paper beats Rock";
+        } else if (computerSelection === "scissors") {
+            return "You Lose! Scissors beats Paper";
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "You Won! Scissors beats Paper";
+        } else if (computerSelection === "rock") {
+            return "You Lose! Rock beats Scissors";
+        }
+    }
+    return "Tied!";
+}
