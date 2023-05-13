a107b2ec3d9b820e48b2f80fb4ede9a1584f699a
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-05-13 18:31:17 -0400

    Create a game function to play 5 rounds that keeps track of the score.

--- javascript.js
+++ javascript.js
@@ -24,16 +24,45 @@ function playRound(playerSelection, computerSelection) {
     }
     else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
-            return "You Won! Paper beats Rock";
+            return "You Win! Paper beats Rock";
         } else if (computerSelection === "scissors") {
             return "You Lose! Scissors beats Paper";
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
-            return "You Won! Scissors beats Paper";
+            return "You Win! Scissors beats Paper";
         } else if (computerSelection === "rock") {
             return "You Lose! Rock beats Scissors";
         }
     }
     return "Tied!";
 }
+
+function game() {
+    let userScore = 0;
+    let computerScore = 0;
+    let userChoice = "";
+    for (let i = 0; i < 5; i++) {
+        userChoice = prompt("Do you choose Rock, Paper, or Scissors? ");
+        computerChoice = getComputerChoice();
+        result = playRound(userChoice, computerChoice);
+        console.log("User's choice: " + userChoice);
+        console.log("Computer: " + computerChoice);
+        console.log(result);
+        if (result.substring(0, 8) === "You Win!") {
+            userScore++;
+        } else if (result.substring(0, 9) === "You Lose!") {
+            computerScore++;
+        }
+        console.log("User's Score: " + userScore);
+        console.log("Computer's Score: " + computerScore);
+    }
+    if (userScore > computerScore) {
+        console.log("You are the winner!");
+    } else if (userScore < computerScore) {
+        console.log("Try again, the computer won!");
+    } else {
+        console.log("Play again, it's a tie score!");
+    }
+}
+console.log(game());
