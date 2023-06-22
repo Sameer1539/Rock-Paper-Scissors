684f7c0be06124fdbc4907c56697a349f8a5800f
Sameer <118702965+Sameer1539@users.noreply.github.com>
2023-06-22 01:25:07 -0400

    Display the running score, and announce a winner once one player reaches 5 points.

--- javascript.js
+++ javascript.js
@@ -39,16 +39,32 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game() {
+    rockBtn.addEventListener('click', () => roundResult.textContent = `${playRound("Rock", getComputerChoice())}`);
+    paperBtn.addEventListener('click', () => roundResult.textContent = `${playRound("Paper", getComputerChoice())}`);
+    scissorsBtn.addEventListener('click', () => roundResult.textContent = `${playRound("Scissors", getComputerChoice())}`);
     let userScore = 0;
     let computerScore = 0;
-    let userChoice = "";
-    if (userScore > computerScore) {
-        console.log("You are the winner!");
-    } else if (userScore < computerScore) {
-        console.log("Try again, the computer won!");
-    } else {
-        console.log("Play again, it's a tie score!");
-    }
+
+    const buttons = document.querySelectorAll('button');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', () => {
+            if (roundResult.textContent.substring(0, 8) === "You Win!") {
+                userScore++;
+            } else if (roundResult.textContent.substring(0, 9) === "You Lose!") {
+                computerScore++;
+            }
+            runningScore.textContent = `User's Score: ${userScore}; Computer's Score: ${computerScore}`;
+            if (userScore == 5 || computerScore == 5) {
+                if (userScore > computerScore) {
+                    winner.textContent = "You are the winner!";
+                } else if (userScore < computerScore) {
+                    winner.textContent = "Try again, the computer won!";
+                }
+                return;
+            }
+        });
+    });
 }
 
 const body = document.querySelector('body');
@@ -56,19 +72,22 @@ const body = document.querySelector('body');
 const rockBtn = document.createElement('button');
 const paperBtn = document.createElement('button');
 const scissorsBtn = document.createElement('button');
-const div = document.createElement('div');
+const roundResult = document.createElement('div');
+const runningScore = document.createElement('p');
+const winner = document.createElement('p');
 
-rockBtn.addEventListener('click', () => div.textContent = `${playRound("Rock", getComputerChoice())}`);
-paperBtn.addEventListener('click', () => div.textContent = `${playRound("Paper", getComputerChoice())}`);
-scissorsBtn.addEventListener('click', () => div.textContent = `${playRound("Scissors", getComputerChoice())}`);
 
 rockBtn.textContent = "Rock";
 paperBtn.textContent = "Paper";
 scissorsBtn.textContent = "Scissors";
 
-
 body.appendChild(rockBtn);
 body.appendChild(paperBtn);
 body.appendChild(scissorsBtn);
-body.appendChild(div);
+body.appendChild(roundResult);
+
+game();
+
+body.appendChild(runningScore);
+body.appendChild(winner);
 
